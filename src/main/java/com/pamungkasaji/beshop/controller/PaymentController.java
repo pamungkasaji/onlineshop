a434f35212e2e68953d04db7ef528a7daea1a04f
pamungkasaji <pamungkasaji1997@gmail.com>
2021-01-28 10:47:06 +0700

    coba

--- src/main/java/com/pamungkasaji/beshop/controller/PaymentController.java
+++ src/main/java/com/pamungkasaji/beshop/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.midtrans.ConfigFactory;
 import com.midtrans.httpclient.error.MidtransError;
 import com.midtrans.service.MidtransCoreApi;
 import com.midtrans.service.MidtransSnapApi;
+import com.pamungkasaji.beshop.entity.OrderEntity;
 import com.pamungkasaji.beshop.entity.PaymentEntity;
 import com.pamungkasaji.beshop.security.SecurityConstants;
 import com.pamungkasaji.beshop.service.OrderService;
@@ -14,13 +15,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@RequestMapping("/api/payment")
 @RestController
 public class PaymentController {
 
@@ -55,22 +54,43 @@ public class PaymentController {
                     false))
             .getCoreApi();
 
-    @GetMapping("/api/config/paypal")
-    public String getMidtransClientInfo(){
-        return SecurityConstants.getPaypalClientId();
+//    @GetMapping("/api/config/paypal")
+//    public String getMidtransClientInfo(){
+//        return SecurityConstants.getPaypalClientId();
+//    }
+//
+//    // COBA
+//    // API `/charge` for mobile SDK to get Snap Token
+//    @PostMapping(value = "/api/orders/checkout", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public Object createTokeSnap(@RequestBody Map<String, Object> body) throws MidtransError {
+//        JSONObject result = snapApi.createTransaction(body);
+//        result.put("clientKey", MIDTRANSCLIENTKEY);
+//        return result.toString();
+//    }
+
+//    status_code=200&transaction_status=settlement&merchant_id=M305004&order_id=ea625e1b-648c-4778-8ff8-85cdfba8a8e9
+//    @PostMapping(value = "/api/payment/notification")
+//    public void transactionFinish(@RequestParam(value = "status_code") int statusCode,
+//                                  @RequestParam(value = "transaction_status") String transactionStatus,
+//                                  @RequestParam(value = "order_id") String orderId) {
+//
+//
+//    }
+
+    @PutMapping(value = "/{id}/pay", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<OrderEntity> midtransPay(@PathVariable String id) {
+
+        return new ResponseEntity<>(orderService.updateDelivered(id), HttpStatus.OK);
     }
 
-    // COBA
-    // API `/charge` for mobile SDK to get Snap Token
-    @PostMapping(value = "/api/orders/checkout", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Object createTokeSnap(@RequestBody Map<String, Object> body) throws MidtransError {
-        JSONObject result = snapApi.createTransaction(body);
-        result.put("clientKey", MIDTRANSCLIENTKEY);
-        return result.toString();
+    @PostMapping(value = "/transactions/status", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> checkTransaction(@RequestBody Map<String, String> transaction) throws MidtransError {
+        JSONObject result = coreApi.checkTransaction(transaction.get("transaction_id"));
+        return new ResponseEntity<>(result.toString(), HttpStatus.OK);
     }
 
     // Midtrans Handling Notification
-    @PostMapping(value = "/api/payment/notification", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/notification", produces = MediaType.APPLICATION_JSON_VALUE)
     private ResponseEntity<String> handleNotification(@RequestBody Map<String, Object> response) throws MidtransError {
         String notifResponse = null;
         if (!(response.isEmpty())) {
@@ -106,4 +126,6 @@ public class PaymentController {
         }
         return new ResponseEntity<>(notifResponse, HttpStatus.OK);
     }
+
+
 }
